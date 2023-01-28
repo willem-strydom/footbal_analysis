0f4e9f02b93a0c0dd54ca9de4a267020a2a1d078
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2023-01-28 01:43:18 +0000

    test faker installed

--- tests/test_hw1.py
+++ tests/test_hw1.py
@@ -1,5 +1,10 @@
 from pathlib import Path
+from importlib.util import find_spec
 
 
 def test_module_exists():
     assert Path("fake_records.py").exists()
+
+
+def test_faker_installed():
+    assert find_spec("faker")
