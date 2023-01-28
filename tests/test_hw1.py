4eab87a357ed15887c5bb94a7280f030e292928a
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2023-01-28 02:26:15 +0000

    test gitignore

--- tests/test_hw1.py
+++ tests/test_hw1.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from importlib.util import find_spec
+from git import Repo
+import os
 
 
 def test_module_exists():
@@ -8,3 +10,10 @@ def test_module_exists():
 
 def test_faker_installed():
     assert find_spec("faker")
+
+
+def test_data_folder_ignored():
+    r = Repo(".")
+    os.mkdir("data")
+    open("data/test", "wb").close()
+    assert not r.untracked_files
