408761e9df6160340eb833f29086741bf5f46263
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2023-01-28 03:20:27 +0000

    add some tests

--- tests/test_hw1.py
+++ tests/test_hw1.py
@@ -8,6 +8,11 @@ try:
 except:
     pass
 
+try:
+    import pandas as pd
+except:
+    pass
+
 
 def test_module_exists():
     assert Path("fake_records.py").exists()
@@ -26,11 +31,36 @@ def test_generate():
         "Birthday",
         "Email",
         "Phone Number",
-    } == df.columns()
+    } == set(df.columns)
 
 
 def test_data_folder_ignored():
     r = Repo(".")
-    os.mkdir("data")
+    if not os.path.exists("data"):
+        os.mkdir("data")
     open("data/test", "wb").close()
     assert not r.untracked_files
+    os.remove("data/test")
+    os.rmdir("data")
+
+
+def test_save():
+    df = pd.DataFrame({"a": [1]})
+    fake_records.save(df)
+    assert os.path.exists("data/fake_records.csv")
+    os.remove("data/fake_records.csv")
+    os.rmdir("data")
+
+
+def test_load(tmp_path):
+    df = pd.DataFrame({"a": [1]})
+    df.to_csv(tmp_path / "test.csv")
+    assert fake_records.load("test.csv").loc["a"] == 1
+
+
+def test_assign_salaries():
+    df = pd.DataFrame({"a": [1]})
+    df = fake_records.assign_salaries(df)
+    df["Salary"].min() >= 20000
+    df["Salary"].max() <= 100000
+    assert len(df.columns) == 2
